d083c6b3908e5ae1e618b09e878c19fe6462a4b9
arete.hu <756148873@qq.com>
2018-03-22 18:30:18 +0800

    Trimed useless codes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.less';
 // 1. Initialize
 const app = dva({
   history: createHistory({
-    basename: 'memo',
+    // basename: 'memo',
   }),
   onError(e) {
   },
--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import { connect } from 'dva';
-import { ToastContainer } from 'react-toastify';
-import Memo from '../memo/memo';
-import styles from './IndexPage.less';
 
 function IndexPage() {
   return (
     <div>
-      <div className={styles.bg} />
-      <div className={styles.content}>
-        <Memo />
-        <ToastContainer autoClose={5000} />
-      </div>
+      123
     </div>
   );
 }

79197bcb6e12e8d621df1a7b0c824f00eb067ef9
arete.hu <756148873@qq.com>
2018-03-29 15:48:27 +0800

    asesome调整

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -5,7 +5,7 @@ import ControlledReactTreeView from '../components/react-treeview/react-treeview
 import DraggableTree from '../components/rc-tree/draggable-tree';
 import BasicTree from '../components/rc-tree/basic-tree';
 import ZTree from '../components/ztree-for-react/ztree';
-import ZTreeDraggable from '../components/ztree-for-react/ztree-draggable';
+import ZTreeTheme from '../components/ztree-for-react/ztree-theme';
 import styles from './IndexPage.less';
 
 function IndexPage() {
@@ -21,7 +21,7 @@ function IndexPage() {
       {/*<hr />*/}
       {/*<ZTree />*/}
       {/*<hr />*/}
-      <ZTreeDraggable />
+      <ZTreeTheme />
     </div>
   );
 }
